9669e28d3d6e78b205b2c434505a492c32426437
Guilherme <47395034+guig3@users.noreply.github.com>
2021-09-13 18:54:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<iframe class="button-api-frame" src="https://youtubemp3api.com/@api/button/mp3/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+<iframe class="button-api-frame" src="https://api.tubemp3.biz/audio/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
 
 <!-- Optional script that automatically makes iframe content responsive. -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"></script>
